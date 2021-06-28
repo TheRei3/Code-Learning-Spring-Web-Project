8e16a11be037d5c9764f8cb56977cf83ac785a8c
admin <admin@DESKTOP-8L2IG92>
2021-06-28 13:51:22 +0900

    코드로 배우는 스프링 웹 프로젝트 part04

---
+++ ex03/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,41 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+	static {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}  //  static
+	
+	@Test
+	public void testConnecttion() {
+
+		try (Connection con =
+			DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:XE"
+					,"book_ex"
+					,"book_ex")){
+			
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+	}  // testConnection()
+	
+}
